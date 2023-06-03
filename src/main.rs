d3fb96618a1030b1ce4ad3692f952d0997290a9c
mohit <mohitpardeshi729@gmail.com>
2023-06-03 23:49:46 +0530

    edited the cargo file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-mod routes;
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
 
-fn main() {
-    rocket::ignite().mount("/", routes![routes::hello]).launch();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
